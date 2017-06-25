1d8f52cd872a385245a701f0e73ebef7b0fbdc36
rgarro <rolando@emptyart.xyz>
2017-06-25 15:15:08 -0600

    Earth angel will you be mine ...

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		
+
+	public float speed;
+	private Rigidbody rb;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
 	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * this.speed);
+	}
+
+
 }
