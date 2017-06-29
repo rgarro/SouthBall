9b8fc40bf266a129151f14a0584ada83c00a08af
rgarro <rolando@emptyart.xyz>
2017-06-29 05:16:23 -0600

    wheres my pickUp ...

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -3,14 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotator : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
 	}
 }
